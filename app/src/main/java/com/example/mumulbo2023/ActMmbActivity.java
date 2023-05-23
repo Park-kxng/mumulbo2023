bb083ef09688d7d581a6a29d5bd86df9386d27a5
Yujin Jeong <yujin45@sookmyung.ac.kr>
2023-05-23 20:40:04 +0900

    기본앱 다 열림, 근데 카톡, 쿠팡은 따로 열림

--- app/src/main/java/com/example/mumulbo2023/ActMmbActivity.java
+++ app/src/main/java/com/example/mumulbo2023/ActMmbActivity.java
@@ -1,11 +1,18 @@
 package com.example.mumulbo2023;
+
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.content.Intent;
@@ -16,6 +23,7 @@ import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,17 +34,19 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Locale;
 
 public class ActMmbActivity extends Activity {
     Button kakaoButton, alarmButton, coupangButton, recordButton;
     //////
-    TextView answerText, recordingText; // TTS로 음성으로 나오기도 하지만 텍스트로도 표시하기 위함
+    TextView recordingText; // TTS로 음성으로 나오기도 하지만 텍스트로도 표시하기 위함
+    //TextView answerText;
     // STT 녹음 관련
     SpeechRecognizer speechRecognizer;
     Intent recordIntent;
     boolean recording = false;  //현재 녹음중인지 여부
-    String userSTT=""; // 음성 인식 결과
+    String userSTT = ""; // 음성 인식 결과
     EditText editText; // ▶ STT 잘 되고 있는지 확인 용도 최종 결과에선 삭제해도 됨
     // TTS 텍스트를 음성으로 관련
     TextToSpeech textToSpeech;
@@ -44,19 +54,27 @@ public class ActMmbActivity extends Activity {
 
     // Chat GPT 관련
     Handler handler = new Handler(Looper.getMainLooper());
-    String temp ="";
+    String temp = "";
     String question = "";
 
     boolean answer = false;  //현재 녹음중인지 여부
     boolean mmb_status = false; // 음성봇이 이야기는지 여부
-    String gptTTS=""; // GPT 검색 결과
-    String result_copy ="";
+    String gptTTS = ""; // GPT 검색 결과
+    String result_copy = "";
+    private static final String TAG = "AppPackageNames";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_mmb);
+        // 내 핸드폰 내의 어떤 앱들 깔려 있나 패키지 이름 출력
+        printAllAppPackageNames();
+        // 카카오톡 유튜브 있나 출력
+        printAppPackageNames("카카오톡", "유튜브", "쿠팡", "시계", "갤러리");
+        // Package Name: com.samsung.android.accessibility.talkback
+        // Package Name: com.samsung.android.app.settings.bixby
+        // com.samsung.android.camerasdkservice
+        // com.samsung.android.bixby.agent
 
         kakaoButton = findViewById(R.id.kakaoButton);
         alarmButton = findViewById(R.id.alarmButton);
@@ -65,11 +83,12 @@ public class ActMmbActivity extends Activity {
         // STT 관련
         recordButton = findViewById(R.id.sttButton);
         editText = findViewById(R.id.sttEditText);
+        recordingText = findViewById(R.id.recordingNowTextView);
 
         /* STT  */
-        recordIntent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        recordIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName());
-        recordIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR");   //한국어
+        recordIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        recordIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+        recordIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR");   //한국어
         recordButton.setOnClickListener(new View.OnClickListener() {
                                             @Override
                                             public void onClick(View v) {
@@ -77,7 +96,7 @@ public class ActMmbActivity extends Activity {
                                                 System.out.println(answer);
 
                                                 // answer == true -> 음성봇이 말해야 하는 경우
-                                                if (answer == true){ // 챗 지피티 답변 시작
+                                                if (answer == true) { // 챗 지피티 답변 시작
                                                     /*
                                                     // 캐릭터의 얼굴을 웃는 얼굴로 변경합니다.
                                                     faceImage.setImageResource(R.drawable.character_mmb_smile);
@@ -101,7 +120,7 @@ public class ActMmbActivity extends Activity {
 
                                                 }
                                                 // answer == false --> 사람이 말해야 하는 경우
-                                                else if(answer == false){
+                                                else if (answer == false) {
                                                     if (!recording) {   //녹음 시작
                                                         //textToSpeech.stop();
                                                         //textToSpeech.shutdown();
@@ -114,13 +133,12 @@ public class ActMmbActivity extends Activity {
                                                         //faceImage.setImageResource(R.drawable.character_mmb);
 
                                                         // 기존에 있던 애들 초기화
-                                                        answerText.setText("");
+                                                        //answerText.setText("");
                                                         editText.setText("");
-                                                        userSTT ="";
+                                                        userSTT = "";
                                                         gptTTS = "";
 
-                                                    }
-                                                    else {  //이미 녹음 중이면 녹음 중지
+                                                    } else {  //이미 녹음 중이면 녹음 중지
                                                         recording = false;
                                                         stopRecord();
                                                         recordingText.setText("녹음중? NO");
@@ -129,7 +147,50 @@ public class ActMmbActivity extends Activity {
                                                         // 아이콘을 스피커 모양으로 변경합니다.
                                                         //recordButton.setImageResource(R.drawable.icon_speak_mmb);
                                                         answer = true;
-
+                                                        // ★★★★★★★★★★★★★★★★★★★★★★
+                                                        if (userSTT.contains("카카오톡") || userSTT.contains("카톡")) {
+                                                            // 카카오톡 앱을 열기 위한 인텐트 생성
+                                                            Intent intent = getPackageManager().getLaunchIntentForPackage("com.kakao.talk");
+                                                            if (intent != null) {
+                                                                // 카카오톡 앱이 설치되어 있는 경우 앱을 엽니다.
+                                                                startActivity(intent);
+                                                                sendMessage();
+                                                            } else {
+                                                                // 카카오톡 앱이 설치되어 있지 않은 경우 마켓으로 이동합니다.
+                                                                Log.d("카카오톡 설치x", "카카오톡 설치되어 있지 않음 마켓으로 이동");
+                                                                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.kakao.talk"));
+                                                                startActivity(intent);
+                                                            }
+                                                        } else if (userSTT.contains("쿠팡") || userSTT.contains("쇼핑")) {
+                                                            // 쿠팡 앱을 열기 위한 인텐트 생성
+                                                            Intent intent = getPackageManager().getLaunchIntentForPackage("com.coupang.mobile");
+                                                            if (intent != null) {
+                                                                // 쿠팡 앱이 설치되어 있는 경우 앱을 엽니다.
+                                                                //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                                                startActivity(intent);
+                                                            } else {
+                                                                // 쿠팡 앱이 설치되어 있지 않은 경우 마켓으로 이동합니다.
+                                                                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.coupang.mobile"));
+                                                                startActivity(intent);
+                                                            }
+                                                        } else if (userSTT.contains("알람") || userSTT.contains("시계")) {
+                                                            Intent intent = new Intent(Intent.ACTION_MAIN);
+                                                            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                                                            intent.setComponent(new ComponentName("com.sec.android.app.clockpackage", "com.sec.android.app.clockpackage.ClockPackage"));
+                                                            startActivity(intent);
+                                                        }else{
+                                                            String pakageName =  findAppPackageNames(userSTT.split(" "));
+                                                            Intent intent = getPackageManager().getLaunchIntentForPackage( pakageName);
+                                                            if (intent != null) {
+                                                                // 쿠팡 앱이 설치되어 있는 경우 앱을 엽니다.
+                                                                //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                                                startActivity(intent);
+                                                            } else {
+                                                                // 쿠팡 앱이 설치되어 있지 않은 경우 마켓으로 이동합니다.
+                                                                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" +  pakageName));
+                                                                startActivity(intent);
+                                                            }
+                                                        }
 
 
                                                     }
@@ -144,7 +205,7 @@ public class ActMmbActivity extends Activity {
         textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
-                if(status!=android.speech.tts.TextToSpeech.ERROR) {
+                if (status != android.speech.tts.TextToSpeech.ERROR) {
 
                     textToSpeech.setLanguage(Locale.KOREAN);
                 }
@@ -212,6 +273,64 @@ public class ActMmbActivity extends Activity {
 
     }
 
+
+    ////////////
+
+    private void printAllAppPackageNames() {
+        PackageManager packageManager = getPackageManager();
+        List<ApplicationInfo> packages = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        for (ApplicationInfo packageInfo : packages) {
+            String packageName = packageInfo.packageName;
+            Log.d(TAG, "Package Name: " + packageName);
+        }
+    }
+
+
+    private void printAppPackageNames(String... appNames) {
+        PackageManager packageManager = getPackageManager();
+        Log.d(TAG, "==========================================");
+        for (String appName : appNames) {
+            Intent intent = new Intent(Intent.ACTION_MAIN, null);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            intent.setPackage(null);
+
+            List<ResolveInfo> resolveInfoList = packageManager.queryIntentActivities(intent, 0);
+
+            for (ResolveInfo resolveInfo : resolveInfoList) {
+                String packageName = resolveInfo.activityInfo.packageName;
+                String label = resolveInfo.loadLabel(packageManager).toString();
+                if (label.equalsIgnoreCase(appName)) {
+                    Log.d(TAG, "App Name: " + appName + ", Package Name: " + packageName);
+                    break;
+                }
+            }
+        }
+    }
+
+    private String findAppPackageNames(String... appNames) {
+        PackageManager packageManager = getPackageManager();
+        Log.d(TAG, "==========================================");
+        for (String appName : appNames) {
+            Intent intent = new Intent(Intent.ACTION_MAIN, null);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            intent.setPackage(null);
+
+            List<ResolveInfo> resolveInfoList = packageManager.queryIntentActivities(intent, 0);
+
+            for (ResolveInfo resolveInfo : resolveInfoList) {
+                String packageName = resolveInfo.activityInfo.packageName;
+                String label = resolveInfo.loadLabel(packageManager).toString();
+                if (label.equalsIgnoreCase(appName)) {
+                    Log.d(TAG, "App Name: " + appName + ", Package Name: " + packageName);
+                    return packageName;
+                    //break;
+                }
+            }
+        }
+        return "";
+    }
+    ////////
     private void sendMessage() {
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("kakaotalk://kakaotalk.com?uri=qr"));
@@ -228,7 +347,7 @@ public class ActMmbActivity extends Activity {
 
         //recordButton.setText("음성 녹음 변환 중지");
         Toast.makeText(getApplicationContext(), "음성인식 시작", Toast.LENGTH_SHORT).show();
-        speechRecognizer=SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
         speechRecognizer.setRecognitionListener(listener);
         speechRecognizer.startListening(recordIntent);
     }
@@ -313,14 +432,14 @@ public class ActMmbActivity extends Activity {
         //인식 결과가 준비되면 호출
         @Override
         public void onResults(Bundle bundle) {
-            ArrayList<String> matches = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);	//인식 결과를 담은 ArrayList
+            ArrayList<String> matches = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);    //인식 결과를 담은 ArrayList
 
 
-            for (int i = 0; i < matches.size() ; i++) {
+            for (int i = 0; i < matches.size(); i++) {
                 userSTT += matches.get(i);
             }
 
-            editText.setText(userSTT);	//기존의 text에 인식 결과 보여
+            editText.setText(userSTT);    //기존의 text에 인식 결과 보여
             speechRecognizer.startListening(recordIntent);    //녹음버튼을 누를 때까지 계속 녹음해야 하므로 녹음 재개
         }
 
@@ -336,7 +455,8 @@ public class ActMmbActivity extends Activity {
     };
 
     // TTS 관련, Activity 종료시 사용한 TTS 객체 정리
-    @Override public void onDestroy() {
+    @Override
+    public void onDestroy() {
         if (textToSpeech != null) {
             textToSpeech.stop();
             textToSpeech.shutdown();
--- app/src/main/java/com/example/mumulbo2023/MainActivity.java
+++ app/src/main/java/com/example/mumulbo2023/MainActivity.java
@@ -142,4 +142,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+
+
 }
